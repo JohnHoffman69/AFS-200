00ade4c53358f2dfa4b3d07806e49e5a9086d530
JohnHoffman69 <john.hoffman@bryanuniversity.edu>
2021-08-09 00:07:09 -0700

    Getting Started with Python

---
+++ week1/getInput.py
@@ -0,0 +1,3 @@
+city = input('What city do you live in? ')
+zip_code = input("what is your zip code? ")
+print("You live in the city of", city, zip_code)
---
+++ week1/useInput.py
@@ -0,0 +1,8 @@
+name = input("What is your name: ")
+age = input("What is your age: ")
+current_year = 2021
+age = int(age)
+diff_years = 100 - age
+age_100 = current_year + diff_years
+
+print(name, "will turn 100 years old in", diff_years, "years from now; in the year", age_100)
