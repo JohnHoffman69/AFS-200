a92e43cd9f2f2d97eaf5afbabdf4dccb9ea5dc03
JohnHoffman69 <john.hoffman@bryanuniversity.edu>
2021-08-16 05:09:36 -0700

    variable, class, numbers

---
+++ week2/variable/variable.py
@@ -0,0 +1,10 @@
+ #variables 
+
+failed_subjects= 2
+name='Matt'
+
+print('Your son ' + name + ' is failing ' + str(failed_subjects) + ' subjects.')
+print(name + '  will need to redo ' + str(failed_subjects) + '  courses.')
+
+name="Paul"
+print(name + '  is doing well in geography.')
